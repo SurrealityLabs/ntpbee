73f63ea415e425469ee4212b799b27a8be62f57b
Randy Glenn <randy@surrealitylabs.com>
2021-01-16 18:10:07 -0500

    Adds customization of NMEA messages sent, and serial data rate

--- ntpbee/nmea_out.ino
+++ ntpbee/nmea_out.ino
@@ -1,6 +1,18 @@
 static char GPSBuffer[128];
+static bool enableZDA;
+static bool enableGGA;
+static bool enableGSA;
+static bool enableRMC;
 
-void gen_nmea(time_t timeUNIX)
+void nmea_init(bool zda, bool gga, bool gsa, bool rmc)
+{
+  enableZDA = zda;
+  enableGGA = gga;
+  enableGSA = gsa;
+  enableRMC = rmc;
+}
+
+void nmea_out(time_t timeUNIX)
 {
   uint32_t start_millis;
   struct tm * timeinfo;
@@ -8,26 +20,38 @@ void gen_nmea(time_t timeUNIX)
   start_millis = millis();
   
   timeinfo = gmtime(&timeUNIX);
-  // FIXME gps message configuration needs to be set in web config
-  snprintf(GPSBuffer, 128, "$GPZDA,%02d%02d%02d.%03d,%02d,%02d,%04d,,*", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, (millis() - start_millis) / 10, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
-  nmea_checksum(GPSBuffer);
-  Serial.println(GPSBuffer);
-  Serial.flush();
-  
-  snprintf(GPSBuffer, 128, "$GPGGA,%02d%02d%02d.%03d,4315.652,N,07955.198,W,1,12,1.0,0.0,M,0.0,M,,*", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, (millis() - start_millis) / 10);
-  nmea_checksum(GPSBuffer);
-  Serial.println(GPSBuffer);
-  Serial.flush();
-  
-  snprintf(GPSBuffer, 128, "$GPGSA,A,3,01,02,03,04,05,06,07,08,09,10,11,12,1.0,1.0,1.0*");
-  nmea_checksum(GPSBuffer);
-  Serial.println(GPSBuffer);
-  Serial.flush();
-  
-  snprintf(GPSBuffer, 128, "$GPRMC,%02d%02d%02d.%03d,A,4315.652,N,07955.198,W,,,%02d%02d%02d,000.0,W*", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, (millis() - start_millis) / 10, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year % 100);
-  nmea_checksum(GPSBuffer);
-  Serial.println(GPSBuffer);
-  Serial.flush();
+
+  if (enableZDA)
+  {
+    snprintf(GPSBuffer, 128, "$GPZDA,%02d%02d%02d.%03d,%02d,%02d,%04d,,*", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, (millis() - start_millis) / 10, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);
+    nmea_checksum(GPSBuffer);
+    Serial.println(GPSBuffer);
+    Serial.flush();
+  }
+
+  if (enableGGA)
+  {
+    snprintf(GPSBuffer, 128, "$GPGGA,%02d%02d%02d.%03d,4315.652,N,07955.198,W,1,12,1.0,0.0,M,0.0,M,,*", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, (millis() - start_millis) / 10);
+    nmea_checksum(GPSBuffer);
+    Serial.println(GPSBuffer);
+    Serial.flush();
+  }
+
+  if (enableGSA)
+  {
+    snprintf(GPSBuffer, 128, "$GPGSA,A,3,01,02,03,04,05,06,07,08,09,10,11,12,1.0,1.0,1.0*");
+    nmea_checksum(GPSBuffer);
+    Serial.println(GPSBuffer);
+    Serial.flush();
+  }
+
+  if (enableRMC)
+  {
+    snprintf(GPSBuffer, 128, "$GPRMC,%02d%02d%02d.%03d,A,4315.652,N,07955.198,W,,,%02d%02d%02d,000.0,W*", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, (millis() - start_millis) / 10, timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year % 100);
+    nmea_checksum(GPSBuffer);
+    Serial.println(GPSBuffer);
+    Serial.flush();
+  }
 }
 
 static void nmea_checksum(char *inp)
--- ntpbee/ntpbee.ino
+++ ntpbee/ntpbee.ino
@@ -20,37 +20,57 @@ const uint8_t ntpIntervalLen = 8;
 char ntpIntervalStr[ntpIntervalLen];
 uint8_t ntpIntervalInt;
 
+const uint8_t serialRateLen = 7;
+char serialRateStr[serialRateLen];
+uint32_t serialRateInt;
+
+const uint8_t nmeaEnableLen = 2;
+char zdaEnableStr[nmeaEnableLen];
+char ggaEnableStr[nmeaEnableLen];
+char gsaEnableStr[nmeaEnableLen];
+char rmcEnableStr[nmeaEnableLen];
+
 bool wifiIsConnected = false;
 
 void wifiConnected();
 void configSaved();
+boolean formValidator();
 
 DNSServer dnsServer;
 WebServer server(80);
 
 IotWebConf iotWebConf(thingName, &dnsServer, &server, wifiInitialApPassword, configVersion);
 IotWebConfParameter ntpServerParam = IotWebConfParameter("NTP Server", "ntpServer", ntpServerStr, ntpServerLen);
-IotWebConfParameter ntpInvervalParam = IotWebConfParameter("NTP Interval (hours)", "ntpInterval", ntpIntervalStr, ntpIntervalLen, "number", "1..168", NULL, "min='1' max='168' step='1'");
+IotWebConfParameter ntpInvervalParam = IotWebConfParameter("NTP Interval (hours)", "ntpInterval", ntpIntervalStr, ntpIntervalLen, "number", "1..168", "1", "min='1' max='168' step='1'");
+IotWebConfParameter serialRateParam = IotWebConfParameter("Serial data rate", "serialRate", serialRateStr, serialRateLen, "number", "1200...115200", "9600", "min='1200' max='115200' step='1'");
+IotWebConfParameter zdaEnableParam = IotWebConfParameter("Enable GPZDA", "zdaEnable", zdaEnableStr, nmeaEnableLen, "number", "0..1", "1", "min='0' max='1' step='1'");
+IotWebConfParameter ggaEnableParam = IotWebConfParameter("Enable GPGGA", "ggaEnable", ggaEnableStr, nmeaEnableLen, "number", "0..1", "1", "min='0' max='1' step='1'");
+IotWebConfParameter gsaEnableParam = IotWebConfParameter("Enable GPGSA", "gsaEnable", gsaEnableStr, nmeaEnableLen, "number", "0..1", "1", "min='0' max='1' step='1'");
+IotWebConfParameter rmcEnableParam = IotWebConfParameter("Enable GPRMC", "rmcEnable", rmcEnableStr, nmeaEnableLen, "number", "0..1", "1", "min='0' max='1' step='1'");
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led_pin, OUTPUT);
   pinMode(config_pin, INPUT);
 
-  Serial.begin(9600); // FIXME make this configurable in web config
+  Serial.begin(9600);
 
   iotWebConf.setStatusPin(led_pin, HIGH);
   iotWebConf.setConfigPin(config_pin);
   iotWebConf.addParameter(&ntpServerParam);
   iotWebConf.addParameter(&ntpInvervalParam);
+  iotWebConf.addParameter(&serialRateParam);
+  iotWebConf.addParameter(&zdaEnableParam);
+  iotWebConf.addParameter(&ggaEnableParam);
+  iotWebConf.addParameter(&gsaEnableParam);
+  iotWebConf.addParameter(&rmcEnableParam);
   iotWebConf.setWifiConnectionCallback(&wifiConnected);
   iotWebConf.setConfigSavedCallback(&configSaved);
+  iotWebConf.setFormValidator(&formValidator);
   iotWebConf.init();
 
-  ntpIntervalInt = atoi(ntpIntervalStr);
-  
+  configSaved();
   ntp_init();
-  ntp_config(ntpServerStr, ntpIntervalInt);
 
   server.on("/", handle_root);
   server.on("/config", []{ iotWebConf.handleConfig(); });
@@ -67,8 +87,50 @@ void wifiConnected()
 
 void configSaved()
 {
+  bool zda, gga, gsa, rmc;
+
+  serialRateInt = atoi(serialRateStr);
+  if (serialRateInt == 0) serialRateInt = 9600;
+  Serial.end();
+  Serial.begin(serialRateInt);
   ntpIntervalInt = atoi(ntpIntervalStr);
   ntp_config(ntpServerStr, ntpIntervalInt);
+
+  zda = zdaEnableStr[0] == '1' ? true : false;
+  gga = ggaEnableStr[0] == '1' ? true : false;
+  gsa = gsaEnableStr[0] == '1' ? true : false;
+  rmc = rmcEnableStr[0] == '1' ? true : false;
+  nmea_init(zda, gga, gsa, rmc);
+}
+
+boolean formValidator()
+{
+  boolean valid = true;
+
+  int r = server.arg(serialRateParam.getId()).toInt();
+  switch (r)
+  {
+    case 1200:
+    case 2400:
+    case 4800:
+    case 9600:
+    case 19200:
+    case 38400:
+    case 57600:
+    case 115200:
+    {
+      valid = true;
+      break;
+    }
+    default:
+    {
+      serialRateParam.errorMessage = "Please use a valid serial data rate (1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200)";
+      valid = false;
+      break;
+    }
+  }
+
+  return valid;
 }
 
 void loop() {
@@ -82,7 +144,7 @@ void loop() {
 
     if (ntp_handle(&timeUNIX))
     {
-      gen_nmea(timeUNIX);
+      nmea_out(timeUNIX);
     }
   }
 }
